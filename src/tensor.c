838cb9e51eec0e2b7ac4b54d8acc4ba73fe8ff64
danlee2002 <68606021+danlee2002@users.noreply.github.com>
2024-03-19 18:55:07 -0400

    added matmul, to do add tiling and fix so method uses pointer

--- src/tensor.c
+++ src/tensor.c
@@ -1,6 +1,7 @@
 #include <assert.h>
-#include <stdio.h>
+#include <immintrin.h>
 #include <stdlib.h>
+#include <stdio.h>
 typedef struct {
   int x;
   int y;
@@ -38,7 +39,28 @@ Tensor *zeros(int x, int y) {
   return a;
 };
 
-Tensor *add(Tensor *a, Tensor *b) {
+Tensor *mult(Tensor *a, Tensor *b) {
+  assert(a->y == b->x);
+  float transposed[b->x][b->y];
+  for (int i = 0; i < b->x; i++) {
+    for (int j = 0; j < b->y; j++) {
+      transposed[i][j] = b->data[j][i];
+    }
+  }
+  Tensor* out = zeros(a->x, b->y);
+  for (int i = 0; i < a->x; i++) {
+    for (int j = 0; j < b->y; j++) {
+      float accsum = 0;
+      for (int k = 0; k < b->x; k++) {
+        accsum += a->data[i][k] * transposed[j][k];
+      }
+      out->data[i][j] = accsum;
+    }
+  }
+  return out;
+};
+
+Tensor* add(Tensor *a, Tensor *b) {
   assert(a->x == b->x);
   assert(a->y == b->y);
   Tensor *out = zeros(a->x, a->y);
@@ -51,3 +73,20 @@ Tensor *add(Tensor *a, Tensor *b) {
   };
   return out;
 };
+
+int main() {
+  float test[2][2] = {{1.0f, 1.0f}, {1.0f, 0.0f}};
+  Tensor* a = tensor(2,2, test);
+  Tensor* out = mult(a,a);
+  out = mult(out,a);
+  for (int i =0; i < 2;i++) {
+    for (int j =0; j < 2; j++) {
+      printf("%f ", out -> data[i][j]);
+     
+
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
--- src/tensor.h
+++
@@ -1,6 +0,0 @@
-#ifndef TENSOR_H
-#define TENSOR_H
-struct tensor;
-
-
-#endif
